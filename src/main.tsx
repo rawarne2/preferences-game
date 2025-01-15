c29af626a4a961ba08b289f107c3e46b858e5a2c
Rashaun Warner <rashaunwarner@yahoo.com>
2025-01-15 11:40:15 -0500

    added speed insights for vercel

--- src/main.tsx
+++ src/main.tsx
@@ -3,10 +3,12 @@ import { createRoot } from 'react-dom/client';
 import './index.css';
 import App from './App.tsx';
 import { inject } from '@vercel/analytics';
+import { injectSpeedInsights } from '@vercel/speed-insights';
 
-// Initialize Vercel Analytics in PROD
+// Initialize Vercel Analytics and Speed Insights when running in PROD
 if (import.meta.env.PROD) {
   inject();
+  injectSpeedInsights();
 }
 
 createRoot(document.getElementById('root')!).render(
