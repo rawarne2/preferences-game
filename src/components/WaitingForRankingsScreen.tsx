bbde61585852a97e254dea11fc075a34f9c8ccf0
Rashaun Warner <rashaunwarner@yahoo.com>
2025-06-02 18:01:54 -0400

    removed unused import

--- src/components/WaitingForRankingsScreen.tsx
+++ src/components/WaitingForRankingsScreen.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import { useGameContext } from "../context/GameContext";
 import { ResetGameButton } from "./ResetGameButton";
 
